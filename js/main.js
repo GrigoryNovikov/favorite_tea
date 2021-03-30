c52d3fa17bbc64075e3ab17f86a987f83af22331
Grigory_Novikov <pegas_mnsms@mail.ru>
2021-03-31 00:20:27 +0300

    adding adaptive

--- js/main.js
+++ js/main.js
@@ -1,169 +1,185 @@
 $(document).ready(() => {
 
+
     $('.category').click((e) => {
         let currentElement = $(e.target);
         $('.products-container').hide();//скрываем элементы с опр классом
         let id = currentElement.data('id');//определяем элемент на котором был клик, заносим значение в переменную id
-        $('#' + id).show();//показваем элемент в переменной id
+        let idName = '#' + id;
+        $(idName).show();//показваем элемент в переменной id
 
-        $('#' + id).slick('refresh');//обновляем первый слайдер
+        $('#' + id).slick('setPosition');//обновляем слайдер
 
         $('.category').removeClass('active');
         currentElement.addClass('active');
     });
 
-
-    $('#mix-tea').slick({
-        centerMode: true,
-        centerPadding: '630px',
-        slidesToShow: 1,
-        speed: 800,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1440,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
+$('.products-container').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    dots: true,
+    variableWidth: true,
+    responsive : [
+        {
+            breakpoint: 1024,
+            settings : {
+                dots:false
             }
-        ]
-    });
 
-    $('#white-tea').slick({
-        centerMode: true,
-        centerPadding: '630px',
-        slidesToShow: 1,
-        slidesToScroll: 2,
-        speed: 800,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    mobileFirst: true,
-                    unslick: true,
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+        }
+    ]
+});
 
-    $('#black-tea').slick({
-        centerMode: true,
-        centerPadding: '630px',
-        slidesToShow: 1,
-        speed: 800,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+    // $('.products-container').slick({
+    //     centerMode: true,
+    //     centerPadding: '630px',
+    //     slidesToShow: 1,
+    //     speed: 800,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1440,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: false,
+    //                 centerPadding: '630px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
 
-    $('#oolong-tea').slick({
-        centerMode: true,
-        centerPadding: '630px',
-        slidesToShow: 1,
-        speed: 800,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-
-    $('#green-tea').slick({
-        centerMode: true,
-        centerPadding: '630px',
-        slidesToShow: 1,
-        speed: 800,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+    // $('#white-tea').slick({
+    //     centerMode: true,
+    //     centerPadding: '630px',
+    //     slidesToShow: 1,
+    //     speed: 800,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 mobileFirst: true,
+    //                 unslick: true,
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
+    //
+    // $('#black-tea').slick({
+    //     centerMode: true,
+    //     centerPadding: '630px',
+    //     slidesToShow: 1,
+    //     speed: 800,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
+    //
+    // $('#oolong-tea').slick({
+    //     centerMode: true,
+    //     centerPadding: '630px',
+    //     slidesToShow: 1,
+    //     speed: 800,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
+    //
+    // $('#green-tea').slick({
+    //     centerMode: true,
+    //     centerPadding: '630px',
+    //     slidesToShow: 1,
+    //     speed: 800,
+    //     dots: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
 
     new WOW({
         animateClass: 'animate__animated'
@@ -193,15 +209,101 @@ $(document).ready(() => {
 
     $('#parallax').enllax();
 
-    $('.slider-item-button').click(() => {
-        $('#popup-order-container').css('display', 'flex');
-    });
+    $('.slider-item-description').on('click', '.slider-item-button', () => {
+        $('#popup-order-container').css('visibility', 'visible');
+    })
 
-    $('#form-close, #popup-order-container').click((e) => {
-        if (e.target.id === 'form-close' || e.target.id === 'popup-order-container') {
-            $('#popup-order-container').hide();
+    $('.popup-card-close, #popup-order-container').click((e) => {
+        if (e.target.id === 'form-close' || e.target.id === 'close' || e.target.id === 'popup-order-container') {
+            $('#popup-order-container').css('visibility', 'hidden');
+            $('#popup-thank-container').css('visibility', 'hidden');
         }
     });
+
+    let loader = $('#loader');
+
+    $('#form-button').click(function () {
+        $('.order-form-error').hide();
+        let name = $('#user-name');
+        let weight = $('#user-weight');
+        let phone = $('#user-phone');
+
+        let hasError = false;
+
+        if (!weight.val()) {
+            weight.siblings('.order-form-error').show();
+            weight.css('border-color', 'red');
+            hasError = true;
+        } else {
+            weight.css('border-color', 'rgb(143, 188, 98)');
+        }
+
+        if (!name.val()) {
+            name.siblings('.order-form-error').show();
+            name.css('border-color', 'red');
+            hasError = true;
+        } else {
+            name.css('border-color', 'rgb(143, 188, 98)');
+        }
+        if (!phone.val()) {
+            phone.siblings('.order-form-error').show();
+            phone.css('border-color', 'red');
+            hasError = true;
+        } else {
+            phone.css('border-color', 'rgb(143, 188, 98)');
+        }
+
+        if (!hasError) {
+            // loader.css('display', 'flex')
+            // $.ajax({
+            //     method: "POST",
+            //     url: 'http://itlogia.ru/test/checkout',
+            //     data: {
+            //         name: name.val(),
+            //         weight: weight.val(),
+            //         phone: phone.val()
+            //     }
+            // })
+            $('#popup-order-container').css('visibility', 'hidden');
+            $('#popup-thank-container').css('visibility', 'visible');
+            // .done(function (message) {
+            //     loader.hide();
+            //     if (message.success) {
+            //
+            //     } else {
+            //         alert('Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ!');
+            //     }
+            // });
+        }
+    });
+
+    // Отправка данных на сервер
+    function send(event, php){
+        console.log("Отправка запроса");
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        var req = new XMLHttpRequest();
+        req.open('POST', php, true);
+        req.onload = function() {
+            if (req.status >= 200 && req.status < 400) {
+                json = JSON.parse(this.response); // Ебанный internet explorer 11
+                console.log(json);
+
+                // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+                if (json.result == "success") {
+                    // Если сообщение отправлено
+                    alert("Сообщение отправлено");
+                } else {
+                    // Если произошла ошибка
+                    alert("Ошибка. Сообщение не отправлено");
+                }
+                // Если не удалось связаться с php файлом
+            } else {alert("Ошибка сервера. Номер: "+req.status);}};
+
+// Если не удалось отправить запрос. Стоит блок на хостинге
+        req.onerror = function() {alert("Ошибка отправки запроса");};
+        req.send(new FormData(event.target));
+    }
+
 
 
 });
--- mail.php
+++ mail.php
@@ -1,43 +1,115 @@
 <?php
+//require 'phpmailer/PHPMailer.php';
+//require 'phpmailer/SMTP.php';
+//require 'phpmailer/Exception.php';
+//
+//require_once('phpmailer/PHPMailerAutoload.php');
+//$mail = new PHPMailer;
+//$mail->CharSet = 'utf-8';
+//
+//$email = $_POST['user_email'];
+//
+////$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//
+//$mail->isSMTP();                                      // Set mailer to use SMTP
+//$mail->Host = 'localhost';  				// Specify main and backup SMTP servers
+//$mail->SMTPAuth = true;                               // Enable SMTP authentication
+//$mail->Username = 'discount@favoritetea.h1n.ru'; // логин от почты с которой будут отправляться письма
+//$mail->Password = 'fN,3nZ2KV8c(s$-'; // пароль от почты с которой будут отправляться письма
+//$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+//$mail->Port = 25; // TCP port to connect to
+//
+//$mail->setFrom('discount@favoritetea.h1n.ru'); // от кого будет уходить письмо?
+//$mail->addAddress($email);     // Кому будет уходить письмо
+////$mail->addAddress('ellen@example.com');               // Name is optional
+////$mail->addReplyTo('info@example.com', 'Information');
+////$mail->addCC('cc@example.com');
+////$mail->addBCC('bcc@example.com');
+////$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+////$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+//$mail->isHTML(true);                                  // Set email format to HTML
+//
+//$mail->Subject = "Получение ПРОМОКОДА";
+//$mail->Body    = 'discount';
+//$mail->AltBody = '';
+//
+//if(!$mail->send()) {
+//    echo 'Message could not be sent.';
+//    echo 'Mailer Error: ' . $mail->ErrorInfo;
+//} else {
+//    echo 'Message has been sent';
+//}
+
+
+// Файлы phpmailer
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+// Переменные, которые отправляет пользователь
 
 $email = $_POST['user_email'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'tea.discount@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'fN,3nZ2KV8c(s$-'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Почта:</b> $email<br><br>
+<b>Сообщение: DISCOUNT</b><br>
+";
 
-$mail->setFrom('tea.discount@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress($email);     // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str;
+    };
 
-$mail->Subject = "Заявка с тестового сайта";
-$mail->Body    = 'disc' .$email;
-$mail->AltBody = '';
+    // Настройки вашей почты
+    $mail->Host = 'localhost'; // SMTP сервера вашей почты
+    $mail->Username = 'discount@favoritetea.h1n.ru'; // Логин на почте
+    $mail->Password = 'fN,3nZ2KV8c(s$-'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 25;
+    $mail->setFrom('discount@favoritetea.h1n.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
 
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    echo '<script language="javascript">';
-    echo 'alert("message successfully sent")';
-    echo '</script>';
+    // Получатель письма
+    $mail->addAddress($email);
+
+    // Прикрипление файлов к письму
+    if (!empty($file['name'][0])) {
+        for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+            $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+            $filename = $file['name'][$ct];
+            if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+                $mail->addAttachment($uploadfile, $filename);
+                $rfile[] = "Файл $filename прикреплён";
+            } else {
+                $rfile[] = "Не удалось прикрепить файл $filename";
+            }
+        }
+    }
+// Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+// Проверяем отравленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+    } else {
+        $result = "error";
+    }
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
-?>
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
