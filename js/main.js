687b0b0363368605950c9d02b134639f53cc0e97
Grigory_Novikov <pegas_mnsms@mail.ru>
2021-04-15 19:26:09 +0300

    adding adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,75 @@
 $(document).ready(() => {
 
+    const propertyInscription = document.querySelectorAll('.properties-info .inscription');
+    const propertyBigCircle = document.getElementById('properties-circle');
+    const propertySmallCircle = document.getElementById('properties-circle2');
+    for (let i=0; i<propertyInscription.length; i++) {
+        propertyInscription[i].addEventListener('mouseover', function () {
+            propertyBigCircle.style.transform = 'scale(1.1)';
+            propertySmallCircle.style.transform = 'scale(1.1)';
+        });
+    }
+    for (let i=0; i<propertyInscription.length; i++) {
+        propertyInscription[i].addEventListener('mouseout', function () {
+            propertyBigCircle.style.transform = 'scale(1)';
+            propertySmallCircle.style.transform = 'scale(1)';
+        });
+    }
+
+    const burgerItem = document.getElementById('burger');
+    const logo = document.querySelector('.logo');
+    const header = document.getElementsByTagName("header")[0];
+    const mainMenu = document.getElementById('main-menu');
+    const menuItems = document.querySelectorAll('.main-menu-item');
+    if (burgerItem) {
+        burgerItem.addEventListener("click", function () {
+            burgerItem.classList.toggle('active');
+            logo.classList.toggle('active');
+            header.classList.toggle('active');
+            mainMenu.classList.toggle('active');
+            for (let i = 0; i < menuItems.length; i++) {
+                menuItems[i].classList.toggle('active');
+            }
+        });
+    }
+
+//Прокрутка
+    const menuLinks = document.querySelectorAll('.main-menu-item[data-goto]');
+
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", menuLinkClick);
+        });
+    }
+
+    function menuLinkClick(e) {
+        const menuLink = e.target;
+
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+
+        if (burgerItem.classList.contains('active')) {
+            burgerItem.classList.toggle('active');
+            logo.classList.toggle('active');
+            header.classList.toggle('active');
+            mainMenu.classList.toggle('active');
+
+            for (let i = 0; i < menuItems.length; i++) {
+                menuItems[i].classList.toggle('active');
+            }
+        }
+    }
+
+
+
 
     $('.category').click((e) => {
         let currentElement = $(e.target);
@@ -14,172 +84,21 @@ $(document).ready(() => {
         currentElement.addClass('active');
     });
 
-$('.products-container').slick({
-    centerMode: true,
-    slidesToShow: 1,
-    dots: true,
-    variableWidth: true,
-    responsive : [
-        {
-            breakpoint: 1024,
-            settings : {
-                dots:false
+    $('.products-container').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        dots: true,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    dots: false
+                }
+
             }
-
-        }
-    ]
-});
-
-    // $('.products-container').slick({
-    //     centerMode: true,
-    //     centerPadding: '630px',
-    //     slidesToShow: 1,
-    //     speed: 800,
-    //     dots: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1440,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: false,
-    //                 centerPadding: '630px',
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
-
-    // $('#white-tea').slick({
-    //     centerMode: true,
-    //     centerPadding: '630px',
-    //     slidesToShow: 1,
-    //     speed: 800,
-    //     dots: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 mobileFirst: true,
-    //                 unslick: true,
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
-    //
-    // $('#black-tea').slick({
-    //     centerMode: true,
-    //     centerPadding: '630px',
-    //     slidesToShow: 1,
-    //     speed: 800,
-    //     dots: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
-    //
-    // $('#oolong-tea').slick({
-    //     centerMode: true,
-    //     centerPadding: '630px',
-    //     slidesToShow: 1,
-    //     speed: 800,
-    //     dots: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
-    //
-    // $('#green-tea').slick({
-    //     centerMode: true,
-    //     centerPadding: '630px',
-    //     slidesToShow: 1,
-    //     speed: 800,
-    //     dots: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 arrows: true,
-    //                 centerMode: true,
-    //                 centerPadding: '40px',
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
+        ]
+    });
 
     new WOW({
         animateClass: 'animate__animated'
@@ -219,8 +138,6 @@ $('.products-container').slick({
             $('#popup-thank-container').css('visibility', 'hidden');
         }
     });
-
-    let loader = $('#loader');
 
     $('#form-button').click(function () {
         $('.order-form-error').hide();
@@ -278,12 +195,12 @@ $('.products-container').slick({
     });
 
     // Отправка данных на сервер
-    function send(event, php){
+    function send(event, php) {
         console.log("Отправка запроса");
         event.preventDefault ? event.preventDefault() : event.returnValue = false;
         var req = new XMLHttpRequest();
         req.open('POST', php, true);
-        req.onload = function() {
+        req.onload = function () {
             if (req.status >= 200 && req.status < 400) {
                 json = JSON.parse(this.response); // Ебанный internet explorer 11
                 console.log(json);
@@ -297,13 +214,17 @@ $('.products-container').slick({
                     alert("Ошибка. Сообщение не отправлено");
                 }
                 // Если не удалось связаться с php файлом
-            } else {alert("Ошибка сервера. Номер: "+req.status);}};
+            } else {
+                alert("Ошибка сервера. Номер: " + req.status);
+            }
+        };
 
 // Если не удалось отправить запрос. Стоит блок на хостинге
-        req.onerror = function() {alert("Ошибка отправки запроса");};
+        req.onerror = function () {
+            alert("Ошибка отправки запроса");
+        };
         req.send(new FormData(event.target));
     }
-
 
 
 });
